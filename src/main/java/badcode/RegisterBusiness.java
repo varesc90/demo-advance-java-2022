bc4c877575df3d7533497ed3e4ef97118918683f
varesc <vares.c@gogoprint.com>
2022-02-17 14:50:46 +0700

    refactor by extract method

--- src/main/java/badcode/RegisterBusiness.java
+++ src/main/java/badcode/RegisterBusiness.java
@@ -6,6 +6,19 @@ public class RegisterBusiness {
 
     public Integer register(SpeakerRepository repository, Speaker speaker) {
         Integer speakerId;
+        validateSpeaker(speaker);
+        int exp = speaker.getExp();
+        speaker.setRegistrationFee(getFee(exp));
+        try {
+            speakerId = repository.saveSpeaker(speaker);
+        } catch (Exception exception) {
+            throw new SaveSpeakerException("Can't save a speaker.");
+        }
+
+        return speakerId;
+    }
+
+    private void validateSpeaker(Speaker speaker) {
         String[] domains = {"gmail.com", "live.com"};
 
         if(speaker == null){
@@ -24,15 +37,6 @@ public class RegisterBusiness {
         if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() != 1) {
             throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
         }
-        int exp = speaker.getExp();
-        speaker.setRegistrationFee(getFee(exp));
-        try {
-            speakerId = repository.saveSpeaker(speaker);
-        } catch (Exception exception) {
-            throw new SaveSpeakerException("Can't save a speaker.");
-        }
-
-        return speakerId;
     }
 
     int getFee(int experienceYear) {
