d90433d3326d4404e03eaef4bf7c8fbf8e6869eb
varesc <vares.c@gogoprint.com>
2022-02-17 11:05:30 +0700

    add test

--- src/test/java/demo/HelloWorldTest.java
+++ src/test/java/demo/HelloWorldTest.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class HelloWorldTest {
 
     @Test
-    void hi() {
+    @DisplayName("Test Everything should run normals")
+    void case1() {
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.hi("ming");
+        assertEquals("Hello ming",result);
+    }
+
+    @Test
+    void case2() {
     }
 }
