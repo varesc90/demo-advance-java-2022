c6f3c808e885ab74277ab32a4b12888a15b3245d
varesc <vares.c@gogoprint.com>
2022-02-17 13:27:38 +0700

    firstname is null check test

--- src/test/java/badcode/RegisterBusinessTest.java
+++ src/test/java/badcode/RegisterBusinessTest.java
@@ -9,10 +9,21 @@ class RegisterBusinessTest {
 
     @Test
     @DisplayName("check Speaker = null should throw runtime exception")
-    void register() {
+    void case01() {
         RegisterBusiness business = new RegisterBusiness();
         Exception exception = assertThrows(RuntimeException.class, () -> { business.register(null,null); });
         assertEquals("Speaker is null",exception.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("First name = null, Argument Exception should be thrown")
+    void case02() {
+        RegisterBusiness business = new RegisterBusiness();
+
+
+        Exception exception = assertThrows(ArgumentNullException.class, () -> {  business.register(null, new Speaker()); });
+        assertEquals("First name is required.",exception.getMessage());
 
     }
 }
